068a4b16605cac7ee65f52ba8e6d0acd5681856c
Diego <pol_rubiano@yahoo.es>
2021-01-25 19:07:00 +0100

    Añadido el método división, y la prueba unitaria del mismo. Añadidos a mayores los getters y setters para poder realizar la prueba unitaria

--- GIT/src/git/Calculadora.java
+++ GIT/src/git/Calculadora.java
@@ -8,4 +8,25 @@ public class Calculadora {
 		this.numero1=n1;
 		this.numero2=n2;
 	}
+	
+	public int getNumero1() {
+		return this.numero1;
+	}
+
+	public void setNumero1(int numero1) {
+		this.numero1 = numero1;
+	}
+
+	public int getNumero2() {
+		return this.numero2;
+	}
+
+	public void setNumero2(int numero2) {
+		this.numero2 = numero2;
+	}
+
+	double division(int numero1, int numero2) {
+		double resultado = numero1 / numero2;
+		return resultado;		
+	}
 }
